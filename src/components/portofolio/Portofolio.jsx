9368cf318c6f7eda262dbbb275d930cc3473ec5b
Horia <horia.marinescu12@gmail.com>
2022-09-05 22:47:15 +0300

    mapped the Portofolio-Projects

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import './Portofolio.css'
 
-import IMG1 from '../../assets/fb-clone.jpg'
+import IMG1 from '../../assets/fb-clone.png'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Facebook Clone',
+    github: 'https://github.com/Hvdri/fb-clone2',
+  }
+]
 
 function Portofolio() {
   return (
@@ -10,18 +19,27 @@ function Portofolio() {
         <h2>Portofolio</h2>
 
         <div className='portofolio-container'>
-            <article className='portofolio-item'>
-                <div className='portofolio-item-image'>
-                  <img src={IMG1} alt='fb-clone' />
-                </div>
-
-                <h3>TITLE</h3>
-                {/* REPO: */}
-                <a className='btn' target='_blank' rel="noreferrer" href="https://github.com/Hvdri?tab=repositories">Github</a>
-                {/* DEMO: */}
-                <a className='btn' target='_blank' rel="noreferrer" href="https://github.com/Hvdri?tab=repositories">Live Demo</a>
-
-            </article>
+            {
+              data.map( ({id, image, title, github}) => {
+                return (
+                  <article 
+                  key={id}
+                  className='portofolio-item'>
+                    <div className='portofolio-item-image'>
+                      <a className='btn-primary' 
+                         target='_blank' 
+                         rel="noreferrer" 
+                         href={github}>
+                          <img src={image} alt='fb-clone' />
+                      </a>
+                    </div>
+                    <div className='portofolio-item-cta'>
+                      <h3>{title}</h3>
+                    </div>
+                  </article>
+                )
+              })
+            }
         </div>
 
     </section>
